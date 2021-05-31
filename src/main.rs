70389d6ae08919256263f606bf9042e7ea9e35da
Tyler Holmes <tyler@holmesengineering.com>
2021-05-31 11:24:07 -0700

    anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+use rand::prelude::*;
+
 use raytracing::{
     camera::{Camera, ASPECT_RATIO},
     circle::Circle,
@@ -14,6 +16,7 @@ use raytracing::{
 
 const IMAGE_HEIGHT: usize = 400;
 const IMAGE_WIDTH: usize = (IMAGE_HEIGHT as f32 * ASPECT_RATIO) as usize;
+const SAMPLES_PER_PIXEL: usize = 100;
 
 pub fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color3d {
     if let Some(hit) = world.hit(ray, 0., f32::INFINITY) {
@@ -39,14 +42,23 @@ fn main() {
     // Camera
     let camera = Camera::new();
 
+    let mut rng = rand::thread_rng();
+
     // render the scene
     for y in (0..IMAGE_HEIGHT).rev() {
         for x in 0..IMAGE_WIDTH {
-            let u = (x as f32) / (IMAGE_WIDTH - 1) as f32;
-            let v = (y as f32) / (IMAGE_HEIGHT - 1) as f32;
-            let r = camera.get_ray(u, v);
+            // anti-aliasing
+            let mut pixel_color: Color3d = Color3d::new(0., 0., 0.);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let v = (y as f32 + rng.gen::<f32>()) / (IMAGE_HEIGHT - 1) as f32;
+                let u = (x as f32 + rng.gen::<f32>()) / (IMAGE_WIDTH - 1) as f32;
+                let r = camera.get_ray(u, v);
 
-            let pixel_color = ray_color(&r, &hitlist);
+                let sub_pixel_color = ray_color(&r, &hitlist);
+                pixel_color = pixel_color + sub_pixel_color;
+            }
+
+            pixel_color = pixel_color / SAMPLES_PER_PIXEL as f32;
             p3.write_color(&pixel_color).unwrap();
         }
     }
